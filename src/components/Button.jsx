b4efc8a2823180d08a1fb189a5f08313a9cbf7b4
ligiabicalho <108960742+ligiabicalho@users.noreply.github.com>
2022-10-27 18:45:05 -0300

    refatorando a requisição da API com async/await

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchAPI } from '../redux/actions';
+import { actionFetchAPI } from '../redux/actions';
 
 class Button extends Component {
   render() {
     const { dispatch } = this.props;
     return (
-      <button type="button" onClick={() => dispatch(fetchAPI())}>
+      <button type="button" onClick={() => dispatch(actionFetchAPI())}>
         Find a cat!
       </button>
     );
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,11 +5,15 @@ export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
-export function fetchAPI() {
-  return (dispatch) => {
+const fetchAPI = async (dispatch) => {
+  try {
     dispatch(requestAPI());
-    return fetch('https://aws.random.cat/meow')
-      .then((response) => response.json())
-      .then((json) => dispatch(getPicture(json)));
-  };
-}
+    const response = await fetch('https://aws.random.cat/meow');
+    const cat = await response.json();
+    dispatch(getPicture(cat));
+  } catch (error) {
+    console.log('erro!');
+  }
+};
+
+export const actionFetchAPI = () => fetchAPI;
